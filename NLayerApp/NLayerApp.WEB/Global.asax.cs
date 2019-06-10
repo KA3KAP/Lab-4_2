8dada2c080add906683e3273aebf20eb670f50d9
polinovadim13@ukr.net <polinovadim13@ukr.net>
2019-06-10 22:29:35 +0300

    Creation of Presentations

--- NLayerApp/NLayerApp.WEB/Global.asax.cs
+++ NLayerApp/NLayerApp.WEB/Global.asax.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Ninject;
+using Ninject.Modules;
+using Ninject.Web.Mvc;
+using NLayerApp.BLL.Infrastructure;
+using NLayerApp.WEB.Util;
 
 namespace NLayerApp.WEB
 {
@@ -16,6 +21,12 @@ namespace NLayerApp.WEB
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // внедрение зависимостей
+            NinjectModule parcel_infoModule = new Parcel_InfoModule();
+            NinjectModule serviceModule = new ServiceModule("DefaultConnection");
+            var kernel = new StandardKernel(parcel_infoModule, serviceModule);
+            DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
         }
     }
 }
